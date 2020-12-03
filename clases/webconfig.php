0205a1025eebb36d3b3f01e2ac44a7ac4465477a
NotFerCam <davidfercam69@gmail.com>
2020-12-03 20:42:05 +0100

    commit2. cambio url y contraseña

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,8 +1,8 @@
 <?php
 //define('DNS','mysql:dbname=conciertos;host=node29125-env-9549929.es-1.axarnet.cloud');  
-define('DNS','mysql:dbname=conciertos;host=localhost');  
+define('DNS','mysql:dbname=conciertos;host=node29132-env-8177372.es-1.axarnet.cloud');  
 define('USERNAME','root');  
-define('PASSWORD','toor');
+define('PASSWORD','LBOdol17251');
 ?>
 
 		
